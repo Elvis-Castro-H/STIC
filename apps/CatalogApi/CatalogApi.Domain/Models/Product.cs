f955ff83d8a13ebc1b8ce409cff2e1d16784d2bf
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-04-17 09:10:41 -0400

    feat: domain layer

---
+++ apps/CatalogApi/CatalogApi.Domain/Models/Product.cs
@@ -0,0 +1,14 @@
+namespace CatalogApi.Domain.Models;
+
+public class Product : BaseEntity<int>
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ImageUrl { get; set; }
+    public int CategoryId { get; set; } 
+    public int Stock { get; set; }
+    public string Details { get; set; } 
+    
+    public Category Category { get; set; }
+}
