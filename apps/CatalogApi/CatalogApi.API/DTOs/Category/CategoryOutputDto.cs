75a384eb8c3cceb0c504bbbd767b18b65d5ba9b7
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-05-19 18:01:04 -0400

    fix: dtos updated based on model

--- apps/CatalogApi/CatalogApi.API/DTOs/Category/CategoryOutputDto.cs
+++ apps/CatalogApi/CatalogApi.API/DTOs/Category/CategoryOutputDto.cs
@@ -7,5 +7,5 @@ public class CategoryOutputDto : IMapFrom<Domain.Models.Category>
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string ImageUrl { get; set; }
+    public string Image { get; set; }
 }
--- apps/CatalogApi/CatalogApi.Domain/Models/Category.cs
+++ apps/CatalogApi/CatalogApi.Domain/Models/Category.cs
@@ -4,7 +4,7 @@ public class Category : BaseEntity<int>
 {
     public string Name { get; set; }
     public string Description { get; set; }
-    public string ImageUrl { get; set; }
+    public string Image { get; set; }
     
     public ICollection<Product> Products { get; set; }
 }
