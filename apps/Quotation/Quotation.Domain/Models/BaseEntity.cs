aa00641c51862cb843906cbb66f79c45ea54d978
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-06-02 09:22:15 -0400

    feat: data layer

---
+++ apps/Quotation/Quotation.Domain/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+namespace Quotation.Domain.Models;
+
+public abstract class BaseEntity <TId>
+{
+    public required TId Id { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
