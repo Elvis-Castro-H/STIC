ee1f6ccb34c66d88e8b190efb4ac3f46241e01d6
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-06-03 19:11:48 -0400

    fix: deploy dependencies conflicts solved

--- apps/Quotation/Quotation.API/Dtos/StandardPrice/StandardPriceOutputDto.cs
+++
@@ -1,7 +0,0 @@
-namespace Quotation.API.Dtos.StandardPrice;
-
-public class StandardPriceOutputDto : StandardPriceInputDto
-{
-    public required int Id { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
