807595fb38a80e2424d3b51cd022ce5362190348
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-06-03 20:56:19 -0400

    fix: gitignore updated with migrations

---
+++ apps/Quotation/Quotation.Data/Migrations/20250521162028_InitialCreate.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Quotation.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Material",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PricePerKg = table.Column<double>(type: "double precision", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Density = table.Column<double>(type: "double precision", nullable: false),
+                    PricePerHourMachine = table.Column<double>(type: "double precision", nullable: false),
+                    PricePerHourOperator = table.Column<double>(type: "double precision", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Material", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gear",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    TeethCount = table.Column<int>(type: "integer", nullable: false),
+                    Module = table.Column<double>(type: "double precision", nullable: false),
+                    PitchDiameter = table.Column<double>(type: "double precision", nullable: false),
+                    OuterDiameter = table.Column<double>(type: "double precision", nullable: false),
+                    Width = table.Column<double>(type: "double precision", nullable: false),
+                    GearType = table.Column<int>(type: "integer", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MaterialId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(450)", unicode: false, maxLength: 450, nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gear", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Gear_Material_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Material",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pulley",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OuterDiameter = table.Column<double>(type: "double precision", nullable: false),
+                    InnerBoreDiameter = table.Column<double>(type: "double precision", nullable: false),
+                    Width = table.Column<double>(type: "double precision", nullable: false),
+                    GrooveCount = table.Column<int>(type: "integer", nullable: false),
+                    GrooveType = table.Column<char>(type: "char(1)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MaterialId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(450)", unicode: false, maxLength: 450, nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pulley", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pulley_Material_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Material",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spacers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    BoltCount = table.Column<int>(type: "integer", nullable: false),
+                    BoltPattern = table.Column<double>(type: "double precision", nullable: false),
+                    ThicknessMm = table.Column<double>(type: "double precision", nullable: false),
+                    CenterBore = table.Column<double>(type: "double precision", nullable: false),
+                    IsHubCentric = table.Column<bool>(type: "boolean", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    MaterialId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "character varying(450)", unicode: false, maxLength: 450, nullable: true),
+                    Price = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spacers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Spacers_Material_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Material",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gear_MaterialId",
+                table: "Gear",
+                column: "MaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pulley_MaterialId",
+                table: "Pulley",
+                column: "MaterialId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Spacers_MaterialId",
+                table: "Spacers",
+                column: "MaterialId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Gear");
+
+            migrationBuilder.DropTable(
+                name: "Pulley");
+
+            migrationBuilder.DropTable(
+                name: "Spacers");
+
+            migrationBuilder.DropTable(
+                name: "Material");
+        }
+    }
+}
