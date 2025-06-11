ed6f884c6ed2459733a9765e28a35a0cba193099
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-06-10 23:33:25 -0400

    feat: gear quotation logic

--- apps/Quotation/Quotation.Application/Abstract/IGearService.cs
+++ apps/Quotation/Quotation.Application/Abstract/IGearService.cs
@@ -4,5 +4,5 @@ namespace Quotation.Application.Abstract;
 
 public interface IGearService : IBaseService<Gear, int>
 {
-    Task<double> CalculatePrice(Gear gear);
+    Task<Gear> CalculatePrice(string material, int toothCount, double module, double pitchDiameter, double outerDiameter, double width, double toothHeight, string gearType);
 }
