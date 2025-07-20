76c346507c53757ccc9ff04e54b91e6e81dbccff
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2025-07-20 11:19:20 -0400

    feat: custom actions

--- apps/intelligent-assistant/actions/actions.py
+++ apps/intelligent-assistant/actions/actions.py
@@ -4,6 +4,9 @@ from rasa_sdk.executor import CollectingDispatcher
 import requests
 import os
 from dotenv import load_dotenv
+from rasa_sdk.events import SlotSet, FollowupAction, UserUtteranceReverted
+from google import genai
+import json
 
 load_dotenv()
 
@@ -37,7 +40,7 @@ class ActionProcesarCotizacionEngranaje(Action):
             "material": material
         }
 
-        url = "https://stic-quotation.onrender.com/api/quotation/Gear/calculate-price"
+        url = "http://localhost:5267/api/quotation/Gear/calculate-price"
         headers = {
             "accept": "text/plain",
             "Content-Type": "application/json"
@@ -73,22 +76,112 @@ class ActionProcesarCotizacionSeparador(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        marca = tracker.get_slot('marca')
-        modelo = tracker.get_slot('modelo')
-        anio = tracker.get_slot('anio')
+        prev_brand = None
+        prev_model = None
+        prev_year = None
+        prev_thickness = None
+        prev_material = None
+        
+        brand = tracker.get_slot("marca")
+        model = tracker.get_slot("modelo")
+        year = tracker.get_slot("anio")
+        thickness = tracker.get_slot("espesor")
+        material = tracker.get_slot("material")
+        
+        api_key = os.getenv("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
 
-        pulgadas = 1.5
-        material = "Aluminio"
+        itr = 0
+
+        while True:
+            itr += 1
+            user_message = tracker.latest_message.get('text')
+
+            prompt = f"""
+            El usuario ha dicho: {user_message}.
+            Extrae la siguiente información en formato JSON con las claves:
+            "brand", "model", "year", "thickness", "material". 
+            considera que para brand, model y material usar el formato Abcd, la primera letra en mayúscula y el resto en minúscula.           
+            para year usar el formato YYYY, para thickness usar el formato X.0 (donde X es un número entero) y debe ser en pulgadas, si recibes en otra unidad lo transformas y lo pasas en pulgadas con solo un decimal
+            si solo proporciona el modelo y eres capaz de deducir la marca, dame también la marca.
+            """
+
+            try:
+                response = client.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=prompt,
+                    config={"response_mime_type": "application/json"}
+                )
+
+                gemini_response = response.text
+                data = json.loads(gemini_response)
+
+                prev_brand = data.get("brand") if data.get("brand") else prev_brand
+                prev_model = data.get("model") if data.get("model") else prev_model
+                prev_year = data.get("year") if data.get("year") else prev_year
+                prev_thickness = data.get("thickness") if data.get("thickness") else prev_thickness
+                prev_material = data.get("material") if data.get("material") else prev_material
+
+            except Exception as e:
+                gemini_response = "Hubo un error procesando tu información. Intenta de nuevo."
+
+            if not brand and prev_brand:
+                brand = prev_brand
+            if not model and prev_model != "0": 
+                model = prev_model
+            if not year and prev_year:
+                year = prev_year
+            if not thickness and prev_thickness:
+                thickness = prev_thickness
+            if not material and prev_material:
+                material = prev_material                
+
+            missing_fields = []
+            if not brand:
+                missing_fields.append("brand")
+            if not model:
+                missing_fields.append("model")
+            if not year:
+                missing_fields.append("year")
+            if not thickness:
+                missing_fields.append("thickness")
+            if not material:
+                missing_fields.append("material")
+
+            if missing_fields:
+                missing_fields_text = ", ".join(missing_fields)
+                request_prompt = f"El usuario ha proporcionado una información parcial para cotizar separadores de aro. Faltan los siguientes datos: {missing_fields_text}. Por favor, genera una pregunta para pedir esa información faltante de manera natural y fluida."                             
+
+                response = client.models.generate_content(
+                    model="gemini-2.5-flash",
+                    contents=request_prompt,
+                    config={"response_mime_type": "application/json"}
+                )
+
+                question_to_ask = response.text
+                dispatcher.utter_message(text=question_to_ask)
+
+                return [FollowupAction("action_listen"),             
+                        SlotSet("marca", brand),
+                        SlotSet("modelo", model),
+                        SlotSet("anio", year),
+                        SlotSet("espesor", thickness),
+                        SlotSet("material", material),
+                        ]  
+            else:
+                break  
+
+        dispatcher.utter_message(text=f"Gracias, toda tu información ha sido procesada correctamente. Marca: {brand}, Modelo: {model}, Año: {year}, Espesor: {thickness}, Material: {material}")
 
         payload = {
-            "make": marca,
-            "model": modelo,
-            "year": int(anio),
-            "inches": pulgadas,
+            "make": brand,
+            "model": model,
+            "year": int(year),
+            "inches": thickness,
             "material": material
         }
 
-        url = "https://stic-quotation.onrender.com/api/quotation/Spacer/calculate-price"
+        url = "http://localhost:5267/api/quotation/Spacer/calculate-price"
         headers = {
             "accept": "text/plain",
             "Content-Type": "application/json"
@@ -102,10 +195,10 @@ class ActionProcesarCotizacionSeparador(Action):
 
             mensaje = (
                 f"He generado una cotización para separadores de aro para su vehículo:\n"
-                f"- Marca: {marca.title()}\n"
-                f"- Modelo: {modelo.title()}\n"
-                f"- Año: {anio}\n"
-                f"- Espesor: {pulgadas}\" pulgadas\n"
+                f"- Marca: {brand.title()}\n"
+                f"- Modelo: {model.title()}\n"
+                f"- Año: {year}\n"
+                f"- Espesor: {thickness}\" pulgadas\n"
                 f"- Material: {material}\n\n"
                 f"El precio estimado es de **BOB {precio:.2f}** por juego."
             )
@@ -113,8 +206,12 @@ class ActionProcesarCotizacionSeparador(Action):
         except Exception as e:
             print(f"Error al cotizar separador: {e}")
             dispatcher.utter_message(text="Lo siento, ocurrió un error al obtener la cotización del separador.")
-        return []
-
+        return [SlotSet("marca", ""),
+                SlotSet("modelo", ""),
+                SlotSet("anio", ""),
+                SlotSet("espesor", ""),
+                SlotSet("material", ""),]
+        
 class ActionProcesarCotizacionPolea(Action):
     def name(self) -> Text:
         return "action_procesar_cotizacion_polea"
@@ -140,7 +237,7 @@ class ActionProcesarCotizacionPolea(Action):
             "grooveType": tipo_de_canal.upper()
         }
 
-        url = "https://stic-quotation.onrender.com/api/quotation/Pulley/calculate-price"
+        url = "http://localhost:5267/api/quotation/Pulley/calculate-price"
         headers = {
             "accept": "text/plain",
             "Content-Type": "application/json"
